1b7fe60b7acc7b235fa3b738bbabcc384a883abe
emilemuny <emilemuny007@gmail.com>
2015-02-15 19:29:05 -0800

    adding progress/added a new db

---
+++ server/config/plugins.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = [
+    {
+    register: require('good'),
+    options: {
+      reporters:[{
+        reporter: require('good-console'),
+      args: [{log: '*', response: '*', error: '*'}]
+
+    }]
+   }
+  }
+];
---
+++ server/routes/monsters/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var Monster = require('../../models/monster');
+
+module.exports = {
+  handler: function(request, reply) {
+    Monster.find(function(err, monsters){
+      reply.view('templates/monsters/index', {monsters:monsters});
+    });
+
+  }
+};
